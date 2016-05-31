a1cf47531969fd1fc67b738f0c5468213ace52fe
Matt Beall <mbeall@starverte.com>
2016-05-30 23:02:53 -0600

    Change $msx_text_domain to MSX_TEXT_DOMAIN

--- cards/msx-card.php
+++ cards/msx-card.php
@@ -11,24 +11,23 @@
  * Return arguments for registering msx_deck
  */
 function msx_card_post_type_args() {
-  global $msx_text_domain;
   $labels = array(
-    'name'                => _x( 'Cards', 'Post Type General Name', $msx_text_domain ),
-    'singular_name'       => _x( 'Card', 'Post Type Singular Name', $msx_text_domain ),
-    'menu_name'           => __( 'Cards', $msx_text_domain ),
-    'all_items'           => __( 'All cards', $msx_text_domain ),
-    'view_item'           => __( 'View', $msx_text_domain ),
-    'add_new_item'        => __( 'Add New', $msx_text_domain ),
-    'add_new'             => __( 'New', $msx_text_domain ),
-    'edit_item'           => __( 'Edit', $msx_text_domain ),
-    'update_item'         => __( 'Update', $msx_text_domain ),
-    'search_items'        => __( 'Search cards', $msx_text_domain ),
-    'not_found'           => __( 'No cards found', $msx_text_domain ),
-    'not_found_in_trash'  => __( 'No cards found in Trash. Did you check recycling?', $msx_text_domain ),
+    'name'                => _x( 'Cards', 'Post Type General Name', MSX_TEXT_DOMAIN ),
+    'singular_name'       => _x( 'Card', 'Post Type Singular Name', MSX_TEXT_DOMAIN ),
+    'menu_name'           => __( 'Cards', MSX_TEXT_DOMAIN ),
+    'all_items'           => __( 'All cards', MSX_TEXT_DOMAIN ),
+    'view_item'           => __( 'View', MSX_TEXT_DOMAIN ),
+    'add_new_item'        => __( 'Add New', MSX_TEXT_DOMAIN ),
+    'add_new'             => __( 'New', MSX_TEXT_DOMAIN ),
+    'edit_item'           => __( 'Edit', MSX_TEXT_DOMAIN ),
+    'update_item'         => __( 'Update', MSX_TEXT_DOMAIN ),
+    'search_items'        => __( 'Search cards', MSX_TEXT_DOMAIN ),
+    'not_found'           => __( 'No cards found', MSX_TEXT_DOMAIN ),
+    'not_found_in_trash'  => __( 'No cards found in Trash. Did you check recycling?', MSX_TEXT_DOMAIN ),
   );
   $args = array(
-    'label'               => __( 'msx_card', $msx_text_domain ),
-    'description'         => __( 'A card', $msx_text_domain ),
+    'label'               => __( 'msx_card', MSX_TEXT_DOMAIN ),
+    'description'         => __( 'A card', MSX_TEXT_DOMAIN ),
     'labels'              => $labels,
     'supports'            => array( 'title' ),
     'hierarchical'        => false,
