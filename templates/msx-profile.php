cd58405d3c0320135ee15b84b4cc828633a51aa1
Matt Beall <mbeall@starverte.com>
2017-12-31 15:29:28 -0700

    Convert space indents to tabs

--- templates/msx-profile.php
+++ templates/msx-profile.php
@@ -9,16 +9,16 @@
  * Display team profile metadata
  */
 function msx_profile_entry_meta() {
-  $title = msx_profile_meta( 'title' );
-  $email = msx_profile_meta( 'email' );
-  $phone = msx_profile_phone();
+	$title = msx_profile_meta( 'title' );
+	$email = msx_profile_meta( 'email' );
+	$phone = msx_profile_phone();
 
-  $output  = '';
-  $output .= ! empty( $title ) ? '<h4>' . $title . '</h4>' : '';
-  $output .= ! empty( $email ) | ! empty( $phone ) ? '<p>' : '';
-  $output .= ! empty( $email ) ? $email : '';
-  $output .= ! empty( $email ) && ! empty( $phone ) ? '<br>' : '';
-  $output .= ! empty( $phone ) ? $phone : '';
-  $output .= ! empty( $email ) | ! empty( $phone ) ? '</p>' : '';
-  echo $output;
+	$output  = '';
+	$output .= ! empty( $title ) ? '<h4>' . $title . '</h4>' : '';
+	$output .= ! empty( $email ) | ! empty( $phone ) ? '<p>' : '';
+	$output .= ! empty( $email ) ? $email : '';
+	$output .= ! empty( $email ) && ! empty( $phone ) ? '<br>' : '';
+	$output .= ! empty( $phone ) ? $phone : '';
+	$output .= ! empty( $email ) | ! empty( $phone ) ? '</p>' : '';
+	echo $output;
 }
